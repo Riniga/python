02bf7bc9911a5f02272df187f0ce2fb48ebae7b0
Rickard Nisses-Gagnér <rickard@nisses-gagner.se>
2024-11-04 13:10:27 +0100

    Updated and removed files

---
+++ concepts/visualize/visualize/data/team_members.js
@@ -0,0 +1,11 @@
+team_nodes = new vis.DataSet([
+    {"color": "#97c2fc", "id": "bhabani.sahoo@skanska.se", "label": "bhabani.sahoo@skanska.se", "shape": "dot"}, 
+    {"color": "#97c2fc", "id": "jonas.edholm@skanska.se", "label": "jonas.edholm@skanska.se", "shape": "dot"}, 
+    {"color": "#97c2fc", "id": "dayanidhi.mishra@skanska.se", "label": "dayanidhi.mishra@skanska.se", "shape": "dot"}, 
+    {"color": "#97c2fc", "id": "kishore.kadikalasadanandam@skanska.se", "label": "kishore.kadikalasadanandam@skanska.se", "shape": "dot"},
+    {"color": "red", "id": "SE IT Fusion Procurement", "label": "SE IT Fusion Procurement", "shape": "dot"} ]);
+
+team_edges = new vis.DataSet([{"color": "gray", "from": "bhabani.sahoo@skanska.se", "to": "SE IT Fusion Procurement", "value": 0.16666666666666666}, 
+    {"color": "gray", "from": "jonas.edholm@skanska.se", "to": "SE IT Fusion Procurement", "value": 0.1111111111111111}, 
+    {"color": "gray", "from": "dayanidhi.mishra@skanska.se", "to": "SE IT Fusion Procurement", "value": 0.16666666666666666}, 
+    {"color": "gray", "from": "kishore.kadikalasadanandam@skanska.se", "to": "SE IT Fusion Procurement", "value": 0.1111111111111111}]);
